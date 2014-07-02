728a86142f5c2a37317f0c262fba3a63238f550d
Paul Hoffman <nkuitse@nkuitse.com>
2014-07-02 10:22:33 +0530

    start POD; simplify custom matching

--- lib/String/Expando.pm
+++ lib/String/Expando.pm
@@ -14,9 +14,9 @@ sub new {
 sub init {
     my ($self) = @_;
     if (defined $self->{'expando'}) {
-        my $rx = $self->{'expando'};
+        my $rx = qr/(?gc)\G$self->{'expando'}/;
         $self->{'consume_expando'} = sub {
-            m{ \G ($rx) }xgc ? (defined $2 ? $2 : $1) : ()
+            $_ =~ $rx ? (defined $2 ? $2 : $1) : ()
         }
     }
     else {
@@ -27,9 +27,9 @@ sub init {
         };
     }
     if (defined $self->{'literal'}) {
-        my $rx = $self->{'literal'};
+        my $rx = qr/(?gc)\G$self->{'literal'}/;
         $self->{'consume_literal'} = sub {
-            m{ \G $rx }xgc ? ($1) : ()
+            $_ =~ $rx ? ($1) : ()
         }
     }
     else {
@@ -38,9 +38,9 @@ sub init {
         }
     }
     if (defined $self->{'escaped_literal'}) {
-        my $rx = $self->{'escaped_literal'};
+        my $rx = qr/(?gc)\G$self->{'escaped_literal'}/;
         $self->{'consume_escaped_literal'} = sub {
-            m{ \G $rx }xgc ? ($1) : ()
+            $_ =~ $rx ? ($1) : ()
         }
     }
     else {
@@ -134,3 +134,96 @@ sub old_decode {
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+String::Expando - expand %(foo) codes in strings
+
+=head1 SYNOPSIS
+
+    $e = String::Expando->new;
+    print $e->expand('%(foo) %(bar)', { foo => 'Hello', bar => 'world!' }), "\n";
+    print $e->expand(
+        '### %04d(year)-%02d(month)-%02d(day)
+        { year => 2011, month => 3, day => 9 }
+    ), "\n";
+    ### 2011-03-09
+
+=head1 METHODS
+
+=over 4
+
+=item B<new>
+
+    $e = String::Expando->new;
+    $e = String::Expando->new(
+        # "[% foo %]" -> $stash->{foo}
+        'expando' => qr/\[%\s*([^%]+?)\s*%\]/,
+        # "%%" -> "%"
+        'escaped_literal' => qr/%(%)/,
+        # etc.
+        'literal' => qr/(.)/,
+    );
+    $e = String::Expando->new(
+        # "%[.2f]L" => sprintf('%.2f', $stash->{L})
+        'expando' => qr{
+            (?x)
+            %
+                # Optional format string
+                (?:
+                    \[
+                        ([^\]]+)
+                    \]
+                )?
+                # Stash key
+                ( [A-Za-z0-9] )
+        },
+        'stash' => { A => 1, B => 2, ... },
+    );
+
+Create a new expando object.  Arguments allowed are as follows.
+
+=over 4
+
+=item B<stash>
+
+The hash from which expando values are obtained.  An expando C<%(xyz)> expanded
+using stash C<$h> will yield the value of C<$h->{'xyz'}> (or the empty string,
+if the value of C<$h->{'xyz'}> is undefined).
+
+=item B<expando>
+
+The regexp (or simple scalar) to use to identify expando codes when parsing the
+input.  It must contain a capture group for what will become the key into the
+stash.  If it contains two capture groups and $2 is defined (and not empty)
+after matching, the value of $1 will be used with sprintf to produce the final
+output.
+
+The default is:
+
+    qr/
+        (?x)
+        \%
+        ([^%()]*j
+        \(
+            ([^\s()]+)
+        \)
+    /
+
+In other words, C<%(...)> with an optional format string between C<%> and C<(>.
+
+=back
+
+=item B<stash>
+
+    $h = $e->stash;
+    $e->stash(\%hash);
+
+Get or set the stash from which expando values will be obtained.
+
+=back
+
+=cut
+
